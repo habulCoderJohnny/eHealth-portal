59bc36abd64f785f78125998380d0c31d01493af
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-18 00:57:09 +0600

    Self-coding: ternary operator run on 'user-Photo' in dashboard

--- src/PAGES/DASHBOARD/MyAppointments.js
+++ src/PAGES/DASHBOARD/MyAppointments.js
@@ -48,10 +48,14 @@ const MyAppointments = () => {
             <tr>
             <td>
               <div class="flex items-center space-x-3">
+                {/* SELF CODING */}
                 <div class="avatar">
-                  <div class="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 w-100">
-                    <img src={user?.photoURL} alt="Avatar" />
-                  </div>
+
+                    {
+                      user.photoURL ? <div class="rounded-full ring ring-primary ring-offset-base-100 ring-offset-2 w-full"> <img src= {user?.photoURL}  alt=".."/> 
+                      </div> : <div class="bg-primary-focus text-neutral-content rounded-full w-24 text-center"><br /><span class="text-base text-white font-bold block">{user?.displayName}</span> </div>
+                    }
+
                 </div>
                 <div>
                   <div class="font-bold">{a.patientName}</div>
