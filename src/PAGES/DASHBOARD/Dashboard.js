3ac5c600276db67ae29bd05d085e690a99d6aff3
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-17 13:44:17 +0600

    dashboard heading styled & responsive drawer for mobile

--- src/PAGES/DASHBOARD/Dashboard.js
+++ src/PAGES/DASHBOARD/Dashboard.js
@@ -4,22 +4,20 @@ import { Link, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div class="drawer drawer-mobile">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content flex flex-col items-center justify-center">
+            <input id="dashboard-sidebar" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content">
                 {/* Page content here */}
-                <h1 className='text-5xl'>DASHBOARD</h1>
+                <h1 className='stat-value text-blue-500'>DASHBOARD</h1>
+                <h3 className='text-purple-500'>Welcome to your Dashboard</h3>
                 <Outlet></Outlet>
-                <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-
             </div>
             <div class="drawer-side">
-                <label for="my-drawer-2" class="drawer-overlay"></label>
+                <label for="dashboard-sidebar" class="drawer-overlay"></label>
                 <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
                     {/* Sidebar content here */}
                     <li><Link to="/dashboard">Dashboard</Link></li>
                     <li><Link to="/dashboard/review">My Review</Link></li>
                 </ul>
-
             </div>
         </div>
     );
