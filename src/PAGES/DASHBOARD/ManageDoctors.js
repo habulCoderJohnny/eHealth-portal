ee1acfc71942520dc4102041dee87888e62dd25e
habulCoderJohnny <fokruljohnny@gmail.com>
2022-05-19 21:45:28 +0600

    Doctors Data load using 'useQuery' & required accessToken

--- src/PAGES/DASHBOARD/ManageDoctors.js
+++ src/PAGES/DASHBOARD/ManageDoctors.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import Loading from '../SHARED/Loading/Loading';
 
 const ManageDoctors = () => {
+    const {data: doctors, isLoading} = useQuery('doctors', ()=> fetch('http://localhost:5000/doctor',{
+        headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`
+        }
+    }).then(res=>res.json()));
+
+    if (isLoading) {
+        return <Loading></Loading>
+    }
     return (
         <div>
-              <h1 className='text-center text-3xl'>Manage<small className='text-[#007C92]'>Doctor</small></h1>
+              <h1 className='text-center text-3xl'>Manage<small className='text-[#007C92]'> Doctor ({doctors.length})</small></h1>
         </div>
     );
 };
